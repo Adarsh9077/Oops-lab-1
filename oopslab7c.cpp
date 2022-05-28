5b1f021cd5564993a7156bf565e565d125c79772
Adarsh9077 <91820758+Adarsh9077@users.noreply.github.com>
2022-05-28 12:11:11 +0530

    Add files via upload

---
+++ oopslab7c.cpp
@@ -0,0 +1,44 @@
+//      LAB - 7c
+
+//	Copy Constructor
+
+
+/*Program to demonstrate the concept of Copy constructor.*/
+#include <iostream>
+using namespace std;
+class Copy
+{
+    private:
+    int m,n ;
+    public: 
+    Copy(int m1, int n1) 
+    {
+        m=m1;
+        n=n1;
+    }
+    Copy (const Copy &Co)
+    {
+        m=Co.m;
+        n=Co.n;
+    }
+    int getM()
+    {
+        return m;
+    }
+    int getN()
+    {
+        return n;
+    }
+};
+int main()
+{
+    Copy Co(30, 108);
+    Copy Co1 = Co;
+    cout<< "m: "<<Co.getM()<<endl
+        << "n: "<<Co.getN()<<endl;
+        
+    cout<< "Co.m: "<<Co1.getM()<<endl
+        << "Co.n: "<<Co1.getN();
+     
+     return 0;
+}
