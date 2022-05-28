1bf569e3d18894ca2e90a59dbf2bcc4a7791c252
Adarsh9077 <91820758+Adarsh9077@users.noreply.github.com>
2022-05-28 11:54:07 +0530

    Add files via upload

---
+++ oopslab7b.cpp
@@ -0,0 +1,31 @@
+//        LAB - 7b
+//	Parameterized Constructor
+/*Program to demonstrate the concept of Parameterized constructor.*/
+#include <iostream>
+using namespace std;
+class output
+{
+    private:
+    int x,y ;
+    public: 
+    output(int x1, int y1)
+    {
+        x=x1;
+        y=y1;
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+};
+    int main()
+{   
+    output m(567, 625);
+    cout<< "The value of x is : "<<m.getX()<<"\n";
+    cout<< "The value of y is : "<<m.getY();
+    return 0;
+}
