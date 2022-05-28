d8262a7929a2072e8d23eb6ad6a0903dec54cec5
Adarsh9077 <91820758+Adarsh9077@users.noreply.github.com>
2022-05-28 10:05:52 +0530

    Add files via upload

---
+++ oopslab2.cpp
@@ -0,0 +1,21 @@
+//		LAB - 2
+
+//Program to make the use of in line function.
+
+
+#include <iostream>
+using namespace std;
+
+inline int add(int a, int b)
+{
+return a+b;
+}
+
+int main()
+{
+intx,y,res;
+cout<<"Enter the number: ";
+cin>>x>>y;
+res = add(x,y);
+cout<<"Summation of two number is " <<res
+}
