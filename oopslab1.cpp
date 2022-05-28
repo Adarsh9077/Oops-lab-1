493ef529a6fa674f531e1f1f641d93eddd656859
Adarsh9077 <91820758+Adarsh9077@users.noreply.github.com>
2022-05-28 09:44:01 +0530

    Add files via upload

---
+++ oopslab1.cpp
@@ -0,0 +1,37 @@
+/*   */
+
+#include<iostream>
+
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int a[3][3],i,j;
+    
+    cout<<"Enter the matrix element:\n";
+    for (i=0;i<3;i++)
+    {
+        for (j=0;j<3;j++)
+        {
+        
+            cin>>a[i][j] ;   
+        }
+    }
+    
+    
+    cout<<"Matrix is .....\n" ;
+    for(i=0;i<3;++i)
+    
+    {
+        for(j=0;j<3;++j)
+        {
+            cout<<a[i][j]<<"  ";
+        }
+        cout<<endl;
+    } 
+    
+
+return 0;
+}
