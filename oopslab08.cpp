64f295a77f79f58a7ad6909b7bd9102ffe72e0f8
Adarsh9077 <91820758+Adarsh9077@users.noreply.github.com>
2022-05-30 01:04:52 +0530

    Update oopslab08.cpp

--- oopslab08.cpp
+++ oopslab08.cpp
@@ -1,60 +1,55 @@
-/*write a program to create an abstract class named Shape that contains an empty method named number of sides()
-.Provided three class named Trapeziod , Triangle , and Hexagon such that each of the classes inherit the class
-shape Each of the class contain the only method number of sides() that shows the number of sides in the given
-geometrical figures.*/
-
+// A program to demonstrate the concept of destructor..
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
-class Shape
+// Class Array to operate on Arrays.
+class Array
 {
-public:
-    void no_of_sides(void); // Empty method 'no_of_sides()' of class Shape;
-};
+    // Data members --to store Array contents and its size;
+    int *arr;
+    int size;
 
-// class 'Trapezoid' is a class which is publicly derived from the base class 'Shape';
-class Trapeziod : public Shape
-{
 public:
-    void no_of_sides() // class trapezoid inherits the function of its base class i.e class Shape;
+    Array() {} // Default constructor.
+
+    Array(int n = 10) // A parameterized constructor to allocate the Array dynamically -with default size 10;
     {
-        // Defination of 'no_of_sides()' method for Trapezoid class.
-        cout << "No of sides of Trapezoid is " << 4 << endl;
+        size = n;                                // update size;
+        arr = (int *)malloc(size * sizeof(int)); // Allocate the array;
     }
-};
-
-// class 'Triangle' is a class which is publicly derived from the base class 'Shape';
-class Triangle : public Shape
-{
-public:
-    void no_of_sides() // class Triangle inherits the function of its base class i.e class Shape;
+    // Destructor to free the allocated memory
+    ~Array() // Destructor-- No arguments and no return type;
     {
-        // Defination of 'no_of_sides()' method for Triangle class.
-        cout << "No of sides of Triangle is " << 3 << endl;
+        cout << "\narray Cleared !";
+        free(arr); // clear the allocated memory.
     }
+    void printArray(void); // Member functions-to print array contents;
+    void getArray(void);   // member function -to get the array input;
 };
 
-// class 'Hexagon' is a class which is publicly derived from the base class 'Shape';
-class Hexagon : public Shape
+void Array::getArray(void) // getArray() def;
 {
-public:
-    void no_of_sides() // class Hexagon inherits the function of its base class i.e class Shape;
+    for (int i = 0; i < size; i++)
     {
-        // Defination of 'no_of_sides()' method for Hexagon class.
-        cout << "No of sides of Hexagon is " << 6 << endl;
+        cin >> arr[i];
     }
-};
-
-// Driver function..
+}
+void Array::printArray(void) // printArray() def;
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+// Driver function
 int main(void)
 {
-    Trapeziod tz;
-    tz.no_of_sides(); // no_of_sides for trapezoid;
-    Triangle tg;
-    tg.no_of_sides(); // no_of_sides for triangle;
-    Hexagon hx;
-    hx.no_of_sides(); // no_of_sides for hexagon;
+    Array a(4); // Array of size 4 is alloacted in heap memory;
+    cout << "Enter Array of size 4 : ";
+    a.getArray(); // get array input;
+    cout << "\nArray : ";
+    a.printArray(); // print the array input;
+    // Destructor is called and memory is cleared;
 
     return 0;
 }
